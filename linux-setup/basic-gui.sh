b8c8052128d861e12308ead1c5e954fc21c061e0
Zshoham <ShohamZarfti@gmail.com>
2021-08-08 19:05:52 +0300

    added slideshow wallpaper

--- linux-setup/basic-gui.sh
+++ linux-setup/basic-gui.sh
@@ -11,3 +11,9 @@ rm ./code.deb
 
 echo "installing desktop apps"
 flatpak install org.telegram.desktop com.bitwarden.desktop com.discordapp.Discord us.zoom.Zoom com.spotify.Client
+
+echo "downloading desktop wallpaper"
+wget -r -np --cut-dirs=1 --no-parent --reject="index.html*" https://zshoham.github.io/static/wallpaper/
+sudo cp -r zshoham.github.io/wallpaper /usr/share/backgrounds
+rm -rf zshoham.github.io
+sudo dconf write /org/gnome/desktop/background/picture-uti ""'file:///usr/share/wallpaper.xml'"
