89f1090ccc2d55345d23e17c48c295f950b59f2a
Zshoham <ShohamZarfti@gmail.com>
2021-09-28 08:44:08 +0000

    Deploying to gh-pages from @ Zshoham/static@9572ce929b029fb36141d728f95a61e1769f015c 🚀

--- linux-setup/dev-gui.sh
+++ linux-setup/dev-gui.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-echo "installing beekeper studio"
+OFF='\033[0m'
+GREEN='\033[0;32m'
+
+nice_print() {
+	echo "\n$GREEN $1 $OFF"	
+}
+
+nice_print "installing beekeper studio"
 wget --quiet -O - https://deb.beekeeperstudio.io/beekeeper.key | gpg --dearmor | sudo tee /usr/share/keyrings/deb.beekeeperstudio.io-archive-keyring.gpg
-echo "deb https://deb.beekeeperstudio.io stable main" | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list
+nice_print "deb https://deb.beekeeperstudio.io stable main" | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list
 sudo apt update
 sudo apt install beekeeper-studio
 
-echo "installing wireshark"
+nice_print "installing wireshark"
 sudo apt install wireshark
 
-echo "installing flatpak apps"
+nice_print "installing flatpak apps"
 flatpak install flathub rest.insomnia.Insomnia \
  com.axosoft.GitKraken
--- linux-setup/gaming.sh
+++ linux-setup/gaming.sh
@@ -1,7 +1,14 @@
 #!/dev/sh
 
-echo "installing steam"
+OFF='\033[0m'
+GREEN='\033[0;32m'
+
+nice_print() {
+	echo "\n$GREEN $1 $OFF"	
+}
+
+nice_print "installing steam"
 sudo apt install steam
 
-echo "installing lutris"
+nice_print "installing lutris"
 sudo apt install lutris
