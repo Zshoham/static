9572ce929b029fb36141d728f95a61e1769f015c
Zshoham <ShohamZarfti@gmail.com>
2021-09-28 11:43:19 +0300

    added nicer prints to all setup scripts

--- linux-setup/basic-gui.sh
+++ linux-setup/basic-gui.sh
@@ -1,59 +1,65 @@
 #!/bin/sh
 
-echo "removing libreoffice"
-sudo apt remove --purge libreoffice-core
-sudo apt remove --purge libreoffice-common
+OFF='\033[0m'
+GREEN='\033[0;32m'
 
-echo "removing firefox"
-sudo apt remove --purge firefox
+nice_print() {
+	echo "\n$GREEN $1 $OFF"	
+}
 
-echo "updating system"
+nice_print "removing libreoffice"
+sudo apt remove -y --purge libreoffice-core
+sudo apt remove -y --purge libreoffice-common
+
+nice_print "removing firefox"
+sudo apt remove -y --purge firefox
+
+nice_print "updating system"
 sudo apt autoremove
-sudo apt update
 sudo apt upgrade
 
-echo "downloading desktop wallpaper"
+nice_print "downloading desktop wallpaper"
 wget -r -np --cut-dirs=1 --no-parent --reject="index.html*" https://zshoham.github.io/static/wallpaper/
 sudo cp -r zshoham.github.io/wallpaper /usr/share/backgrounds
 rm -rf zshoham.github.io
 dconf write /org/gnome/desktop/background/picture-uri "'file:///usr/share/backgrounds/wallpaper.xml'"
 
-echo "installing synaptic"
+nice_print "installing synaptic"
 sudo apt install synaptic
 
-echo "installing alacritty"
+nice_print "installing alacritty"
 sudo apt install alacritty
 
-echo "installing vscode"
+nice_print "installing vscode"
 curl -fsSLo code.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 sudo apt install ./code.deb
 rm ./code.deb
 
-echo "installing brave browser"
+nice_print "installing brave browser"
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+nice_print "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser
 
-echo "installing stacer"
+nice_print "installing stacer"
 sudo apt install stacer
 
-echo "installing gnome-boxes"
+nice_print "installing gnome-boxes"
 sudo apt install gnome-boxes
 
-echo "installing telegram"
+nice_print "installing telegram"
 sudo apt install telegram-desktop
 
-echo "installing spotify"
+nice_print "installing spotify"
 sudo curl -fsSLo /usr/share/keyrings/spotify-keyring.gpg https://download.spotify.com/debian/pubkey_0D811D58.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+nice_print "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update
 sudo apt install spotify-client
 
-echo "installing gnome drawing"
+nice_print "installing gnome drawing"
 sudo apt install drawing
 
-echo "installing flatpack desktop apps"
+nice_print "installing flatpack desktop apps"
 flatpak flathub com.axosoft.GitKraken \
  flathub com.discordapp.Discord
 
--- linux-setup/writing.sh
+++ linux-setup/writing.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-echo "installing LaTex"
+OFF='\033[0m'
+GREEN='\033[0;32m'
+
+nice_print() {
+	echo "\n$GREEN $1 $OFF"	
+}
+
+nice_print "installing LaTex"
 sudo apt install texlive texlive-publishers texlive-science texlive-pstricks texlive-pictures texlive-xetex texlive-lang-other
 
-
+nice_print "installing Obsidian"
 flatpak install flathub md.obsidian.Obsidian 
