78e630fbd5f663574f93fe5b5a673eea26101dd2
Zshoham <ShohamZarfti@gmail.com>
2021-11-06 10:33:34 +0000

    Deploying to gh-pages from @ Zshoham/static@22f2bd939815f4c7487ce244754ab6c045686bb8 🚀

--- linux-setup/basic.sh
+++ linux-setup/basic.sh
@@ -29,6 +29,13 @@ nice_print "enabling ufw and disabling ssh connections from outside local networ
 sudo ufw enable
 sudo ufw allow from 192.168.0.0/16 to any port 22 proto tcp
 
+nice_print "installing fish"
+sudo apt install fish
+mkdir .config/fish
+
+nice_print "changing default shell to fish"
+sudo chsh -s `which fish`
+
 nice_print "installing jetrains mono font"
 curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh | bash
 curl -fsSL https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -o JetBrainsMono.zip
@@ -37,22 +44,11 @@ cp JetBrainsMono/* $HOME/.local/share/fonts
 rm JetBrainsMono.zip
 rm -rf JetBrainsMono
 
-nice_print "installing github cli"
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh
+nice_print "installing nixpkgs"
+sh <(curl -L https://nixos.org/nix/install) --daemon
+. /etc/profile.d/nix.sh
 
-nice_print "installing micro"
-curl https://getmic.ro | bash
-sudo mv micro /usr/bin
-
-nice_print "installing fish"
-sudo apt install fish
-mkdir .config/fish
-
-nice_print "changing default shell to fish"
-sudo chsh -s `which fish`
+nix-env -i gh micro bat exa ripgrep fd delta du-dust broot bitwarden-cli starship
 
 nice_print "installing rust"
 export CARGO_HOME="$HOME/.local/share/cargo"
@@ -60,23 +56,8 @@ export RUSTUP_HOME="$HOME/.local/share/rustup"
 export PATH="$CARGO_HOME/bin:$PATH"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-nice_print "installing cmd tools"
-$CARGO_HOME/bin/cargo install bat exa ripgrep du-dust procs git-delta fd-find broot
+nice_print "installing jq"
 sudo apt install jq
-
-
-nice_print "installing bitwarden cli"
-bw_url=`curl --silent "https://api.github.com/repos/bitwarden/cli/releases/latest" \
- | jq -r '.assets | .[].browser_download_url' \
- | rg 'bw-linux.*.zip'`
-
-wget -O bw.zip "$bw_url"
-sudo unzip -d /usr/bin bw.zip
-sudo chmod +x /usr/bin/bw
-rm bw.zip
-
-nice_print "installing starship"
-curl -fsSL https://starship.rs/install.sh | sh
 
 nice_print "installing tmux"
 sudo apt install tmux
@@ -92,7 +73,7 @@ nice_print "installing neofetch"
 sudo apt install neofetch
 
 nice_print "installing chezmoi"
-sudo sh -c "$(curl -fsLS git.io/chezmoi) -b /usr/bin"
+nix-env -i chezmoi
 
 cd $HOME
 
